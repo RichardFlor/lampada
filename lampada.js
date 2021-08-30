16233e6207a5c4424ec8cca320e520d24d4a4e1c
RICHARD MATHEUS FLOR DOROTEIO <21194072@CFP127ALUNO.LOCAL>
2021-08-30 16:57:01 -0300

    adicionando o botão PISCAR

--- lampada.js
+++ lampada.js
@@ -32,6 +32,17 @@ function quebrarLampada(){
     lampada.src="img/quebrada.jpg"
     botoesLigaDesliga(true, true)
 }
+function piscar(){
+    var intervalo = 0;
+    var contador = 0;
+    while (contador <10 ){
+        intervalo +=300;
+        setTimeout("document.getElementById('lampada').src='img/ligada.jpg';",intervalo);
+        intervalo +=300;
+        setTimeout("document.getElementById('lampada').src='img/desligada.jpg';",intervalo);
+        contador++;
+    }
+}
 
 //eventos
 document.getElementById("ligar").addEventListener("click", ligarLampada)
@@ -40,3 +51,4 @@ document.getElementById("desligar").addEventListener("click",desligarLampada)
 document.getElementById("lampada").addEventListener("mouseover", ligarLampada)
 document.getElementById("lampada").addEventListener("mouseout", desligarLampada)
 document.getElementById("lampada").addEventListener("dblclick", quebrarLampada)
+document.getElementById("piscar").addEventListener("click", piscar)
