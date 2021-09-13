7724cd369beab0f4b87469641b6c6c972e5bfc69
RICHARD MATHEUS FLOR DOROTEIO <21194072@CFP127ALUNO.LOCAL>
2021-09-13 13:56:00 -0300

    Adicionando o Piscar

--- lampada.js
+++ lampada.js
@@ -1,6 +1,7 @@
 "use strict"
 
 const lampada = document.getElementById("lampada")
+let idInterval 
 
 function lampadaInteira(){
     return !lampada.src.includes("quebrada")
@@ -15,34 +16,53 @@ desligar.disabled = estadoDesliga
 
 }
 
-function ligarLampada(){
+function ligarLampada(evento){
     if(lampadaInteira()){
     lampada.src="img/ligada.jpg"
     botoesLigaDesliga(true, false)
     }
+    if (evento) pararPiscar()
 }
 
-function desligarLampada(){
+function desligarLampada(evento){
     if(lampadaInteira()){
     lampada.src="img/desligada.jpg"
     botoesLigaDesliga(false, true)
     }
+    if (evento) pararPiscar()
 }
+
 function quebrarLampada(){
     lampada.src="img/quebrada.jpg"
     botoesLigaDesliga(true, true)
 }
-function piscar(){
-    var intervalo = 0;
-    var contador = 0;
-    while (contador <10 ){
-        intervalo +=300;
-        setTimeout("document.getElementById('lampada').src='img/ligada.jpg';",intervalo);
-        intervalo +=300;
-        setTimeout("document.getElementById('lampada').src='img/desligada.jpg';",intervalo);
-        contador++;
+function lampadaDesligada(){
+    return lampada.src.includes("desligada")
+}
+function trocarImagem(){
+    if(lampadaDesligada()){
+        ligarLampada()
+    }else{
+    desligarLampada()
     }
 }
+
+function pararPiscar(){
+    clearInterval(idInterval)
+}
+function piscar(){
+    // var intervalo = 0;
+    // var contador = 0;
+    // while (contador <10 ){
+    //     intervalo +=300;
+    //     setTimeout("document.getElementById('lampada').src='img/ligada.jpg';",intervalo);
+    //     intervalo +=300;
+    //     setTimeout("document.getElementById('lampada').src='img/desligada.jpg';",intervalo);
+    //     contador++;
+    pararPiscar()
+    idInterval = setInterval(trocarImagem, 1000)
+    }
+
 
 //eventos
 document.getElementById("ligar").addEventListener("click", ligarLampada)
